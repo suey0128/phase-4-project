7d0b2cf5cb9fd8fdb58c8f547b13e994d04c6272
suey0128 <syui0128@gmail.com>
2021-07-30 18:31:21 -0700

    updated seeding add-on

--- client/src/components/HomeICPressOnFilter.js
+++ client/src/components/HomeICPressOnFilter.js
@@ -2,6 +2,14 @@ function HomeICPressOnFilter() {
     return (
         <div>
             <h2>this is HomeICPressOnFilter</h2>
+
+            <input type="checkbox" id="vehicle1" name="vehicle1" value="Bike"/>
+            <label for="vehicle1"> I have a bike</label>
+            <input type="checkbox" id="vehicle2" name="vehicle2" value="Car"/>
+            <label for="vehicle2"> I have a car</label>
+            <input type="checkbox" id="vehicle3" name="vehicle3" value="Boat"/>
+            <label for="vehicle3"> I have a boat</label>
+
         </div>
     )
   }
