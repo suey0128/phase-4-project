238842306fcac90e71aa3afe9e551017485dd09c
suey0128 <syui0128@gmail.com>
2021-07-30 18:07:21 -0700

    finsihed seeding by Bri, updated HomeSearchAndSort.js and updated presson item card

--- client/src/components/HomeSearchAndSort.js
+++ client/src/components/HomeSearchAndSort.js
@@ -1,7 +1,34 @@
 function HomeSearchAndSort () {
+
+    //need to move to a higher level
+    const onSearchChange = (e) => {
+        console.log(e.target.value)
+    }
+
     return (
         <div>
             <h2>this is HomeSearchAndSort</h2>
+
+            <div className="look-for-div">
+                <p>Looking for:</p>
+                <button>Press On</button>
+                <button>Glue</button>
+                <button>Hand Care</button>
+            </div>
+
+            <div className="search-div">
+                <input id="search-bar" type="text" placeholder="Search..." 
+                    onChange={(e)=>{onSearchChange(e.target.value)}}/>
+                    
+                </div>
+                    <select name="SortBy" id="SortBy">
+                        <option value="BestSelling">Best Selling</option>
+                        <option value="PriceLowtoHigh">Price: Low to High</option>
+                        <option value="PriceHightoLow">Price: High to Low</option>
+                    </select>
+                <div>
+
+            </div>
         </div>
     )
   }
