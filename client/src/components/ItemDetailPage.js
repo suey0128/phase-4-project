f9d2fa29936b64687de0ed6d038378da21bb77c5
suey0128 <syui0128@gmail.com>
2021-08-02 09:26:58 -0700

    resoviing enviroment issues

--- client/src/components/ItemDetailPage.js
+++ client/src/components/ItemDetailPage.js
@@ -1,8 +1,10 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from 'react'
+import shape from "@material-ui/core/styles/shape";
 
-function ItemDetailPage () {
+function ItemDetailPage ({showItemPage}) {
     const [itemInfo, setItemInfo] = useState(null)
+    const [isLoaded, setIsLoaded] = useState(false)
 
     const params = useParams();
     // console.log (params)  => {type: "press_ons", id: "2"}
@@ -15,20 +17,38 @@ function ItemDetailPage () {
             if (res.ok) {
                 const itemData =  await res.json()
                 setItemInfo(itemData)
+                setIsLoaded(true);
+
             }
         }
         fetchItem(itemInfo)
     },[])
+
+    if (!isLoaded) return <h2>Loading...</h2>;
+
     console.log(itemInfo)
 
     return (
 
         <div>
             <h2>this is ItemDetailPage</h2>
-            {/* <img src={itemInfo.image}/>
+            <img src={itemInfo.image}/>
             <h3>{itemInfo.name}</h3>
-            <p>{itemInfo.price}</p>
-            <p>{itemInfo.quantity}</p>  */}
+            <p>$ {itemInfo.price}</p>
+            <p>{itemInfo.quantity}</p> 
+            {
+            //  if (showItemPage === "pressOn") {
+            //  <p>color: <span>shape: </span> <span>add on: </span></p>
+            //  } else if (showItemPage ==="glue") {
+            //     <p>strength: </p>
+            //  } else {
+            // <p>description: </p>
+            //  }
+            showItemPage === "pressOn" ? 
+            ( <p>color: <span>shape: </span> <span>add on: </span></p>
+            ) : null
+            }
+
         </div>
         
     )
